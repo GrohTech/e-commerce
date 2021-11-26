002c0daa9e5aa8f9f94b8060d094780a15d59f6e
Lauren Groh <legroh@uwm.edu>
2021-11-26 11:43:56 -0600

    complete category model

--- models/Category.js
+++ models/Category.js
@@ -1,3 +1,4 @@
+const { stringify } = require('querystring');
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
@@ -6,7 +7,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
