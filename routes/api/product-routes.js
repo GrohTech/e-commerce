2408748c74b3f239347f86b1751124b4bde3806d
Lauren Groh <legroh@uwm.edu>
2021-11-27 12:01:34 -0600

    complete tag-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -28,7 +28,6 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
   Product.findOne({
     attributes: ["id", "product_name", "price", "stock", "category_id"],
     where: {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,24 +5,75 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+    Tag.findAll({
+    attributes: ["id", "tag_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+        through: ProductTag
+      }
+    ],  
+  }).then(dbTagAll => {
+    res.json(dbTagAll);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    attributes: ["id", "tag_name"],
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+        through: ProductTag
+      }
+    ],
+  }).then(dbTagOne => {
+    res.json(dbTagOne);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  }).then(dbTagNew => {
+    res.json(dbTagNew);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then(dbTagUpdate => {
+    res.json(dbTagUpdate);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy(
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then(dbTagDelete => {
+    res.json(dbTagDelete);
+  });
 });
 
 module.exports = router;
